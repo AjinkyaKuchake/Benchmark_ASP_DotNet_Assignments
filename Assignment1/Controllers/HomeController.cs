bb076bca24d9e62d5c07fe126856af1eb9f25ea6
AjinkyaKuchake <97749644+AjinkyaKuchake@users.noreply.github.com>
2022-01-20 00:38:24 +0530

    Add files via upload

---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string data)
+        {
+            string fileName = null;
+            if (data == "Sample")
+            {
+                fileName = "~/" + data + ".pdf";
+                return File(fileName, "application/pdf");
+
+            }
+
+            else if (data == "gotoabout")
+            {
+                return RedirectToAction("About");
+            }
+
+            else if (data == "login")
+            {
+                return View("LoginView");
+            }
+
+            else
+            {
+                return Content("You entered:  "+data);
+            }
+
+        }
+
+        public ActionResult About()
+        {
+            return Content("About Content here");
+        }
+    }
+}
